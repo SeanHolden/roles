393001e3c59c1e9209014e1615e5e5c79a0dcdbf
Sean Holden <seanholden1@gmail.com>
2017-02-06 18:48:34 +0000

    Added new recipes to rails_postgres role

--- rails_postgres.rb
+++ rails_postgres.rb
@@ -1,6 +1,6 @@
 name 'rails_postgres'
 description 'standard rails server with postgres as a database'
-run_list 'recipe[git]', 'recipe[vim]', 'recipe[ruby]', 'recipe[chruby], recipe[postgres]'
+run_list 'recipe[git]', 'recipe[vim]', 'recipe[gcc]', 'recipe[ruby]', 'recipe[chruby]', 'recipe[postgresql]', 'recipe[postgresql::app_setup]'
 # env_run_lists 'production' => ['recipe[nginx::config_prod]'], 'testing' => ['recipe[nginx::config_test]']
 # default_attributes "nginx" => { "log_location" => "/var/log/nginx.log" }
 # override_attributes "nginx" => { "gzip" => "on" }
