a7db83b51101694868837875226e1c9fc3debc0c
Sean Holden <sean.holden@onthebeach.co.uk>
2017-02-14 19:20:44 +0000

    added the_silver_searcher to rails_postgres role

--- rails_postgres.rb
+++ rails_postgres.rb
@@ -1,7 +1,16 @@
 name 'rails_postgres'
 description 'standard rails server with postgres as a database'
 
-run_list 'recipe[git]', 'recipe[vim]', 'recipe[gcc]', 'recipe[ruby]', 'recipe[chruby]', 'recipe[postgresql]', 'recipe[postgresql::app_setup]'
+run_list %w(
+  recipe[git]
+  recipe[vim]
+  recipe[gcc]
+  recipe[ruby]
+  recipe[chruby]
+  recipe[the_silver_searcher]
+  recipe[postgresql]
+  recipe[postgresql::app_setup]
+)
 # env_run_lists 'production' => ['recipe[nginx::config_prod]'], 'testing' => ['recipe[nginx::config_test]']
 # default_attributes "nginx" => { "log_location" => "/var/log/nginx.log" }
 # override_attributes "nginx" => { "gzip" => "on" }
