09520fb26f30b75ced9d573883fa8b95285548f2
Sean Holden <seanholden1@gmail.com>
2017-03-08 17:17:39 +0000

    Added new cookbooks to npm role

--- npm.rb
+++ npm.rb
@@ -2,10 +2,13 @@ name 'npm'
 description 'standard npm dev environment for simple JS projects'
 run_list(
   %w(
+    recipe[ssh]
     recipe[ohmyzsh]
     recipe[git]
+    recipe[the_silver_searcher]
     recipe[vim]
     recipe[gcc]
     recipe[npm]
+    recipe[dev_env]
   )
 )
