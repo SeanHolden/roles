7f7893797b1210c8497fedacadebda0424725a3d
Sean Holden <seanholden1@gmail.com>
2017-08-28 20:43:42 +0100

    added ohmyzsh to gamedev role

--- gamedev.rb
+++ gamedev.rb
@@ -3,6 +3,7 @@ description 'standard JS based gamedev environment for HTML5 phaser gamedev'
 run_list(
   %w(
     recipe[npm::ubuntu]
-    recipe[git::ubuntu]
+    recipe[git::default]
+    recipe[ohmyzsh::default]
   )
 )
